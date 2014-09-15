fb127c8d54437d7b1f46935d4a2aee92c1e17dcb
Eunseo Choi <eunseo.choi@gmail.com>
2014-09-15 11:54:52 -0500

    restructured

---
+++ Snac/bmi/tests/irf_test.c
@@ -0,0 +1,86 @@
+#include <mpi.h>
+#include "StGermain/StGermain.h"
+#include "StGermain/FD/FD.h"
+#include "Snac/Snac.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <bmi.h>
+
+
+int
+main (void)
+{
+    BMI_Model *self = NULL;
+    int err = BMI_SUCCESS;
+    char name[BMI_MAX_COMPONENT_NAME];
+    
+    /***********************/
+    /* Initialization      */
+    /***********************/
+    fprintf (stdout, "Initializing... ");
+    
+    err = BMI_Initialize ("./bmi_test.xml", &self);
+    if (err || !self)
+        return EXIT_FAILURE;
+    
+    fprintf (stdout, "PASS\n");
+
+    /***********************/
+    /* Get component name  */
+    /***********************/
+
+    BMI_Get_component_name (self, name);
+    fprintf (stdout, "%s\n", name);
+
+    /***********************/
+    /* Update              */
+    /***********************/
+    // BMI_Run_model (self);
+
+    {
+        int i;
+        const int n_steps = 10;
+        double time;
+        
+        for (i = 0; i < n_steps; i++)
+            {
+                fprintf (stdout, "Running until t = %d... ", i+1);
+                if (BMI_Update (self)==0 && BMI_Get_current_time (self, &time)==0) {
+                    if (fabs (time-(i+1)) < 1e-6)
+                        fprintf (stdout, "PASS\n");
+                    else {
+                        return EXIT_FAILURE;
+                    }
+                }
+                else
+                    return EXIT_FAILURE;
+            }
+        
+        fprintf (stdout, "Running until t = %f... ", 1000.5);
+        if (BMI_Update_until (self, 1000.5)==0 && BMI_Get_current_time (self, &time)==0) {
+            if (fabs (time-1000.5) < 1e-6)
+                fprintf (stdout, "PASS\n");
+            else {
+                fprintf (stdout, "%f\n", time);
+                return EXIT_FAILURE;
+            }
+        }
+        else
+            return EXIT_FAILURE;
+    }
+    
+    /***********************/
+    /* Finalization        */
+    /***********************/
+    fprintf (stdout, "Finalizing... ");
+    err = BMI_Finalize (self);
+    
+    if (err)
+        return EXIT_FAILURE;
+    fprintf (stdout, "PASS\n");
+    
+    return EXIT_SUCCESS;
+}
